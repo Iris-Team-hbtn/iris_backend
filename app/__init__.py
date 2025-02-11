0d039313c34c59aa9bf4a776accaa8e0a745b0f4
Axel Palombo <140984664+axelpalwo@users.noreply.github.com>
2025-02-11 10:33:43 -0300

    cors update
    Added CORS for testing purposes

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,14 @@
 from flask import Flask
 from flask_restx import Api
+from flask_cors import CORS
 from app.models.model import createVS
 from app.api.v1.iris import api as iris_ns
-
 
 def create_app():
     app = Flask(__name__)
     api = Api(app, version='1.0', title='Iris API', description='Iris Application API')
-
+    
+    CORS(app)
     app.config["vs"] = createVS()
 
     api.add_namespace(iris_ns, path='/iris')
