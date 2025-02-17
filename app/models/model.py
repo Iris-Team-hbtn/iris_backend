300935e80c46ff0cddd810a3d1740b2fa026c1ff
Magnusmajo <dralexis19003227@gmail.com>
2025-02-17 11:32:12 -0300

    correccion de ruta para storageBucket

--- app/models/model.py
+++ app/models/model.py
@@ -8,20 +8,16 @@ firebaseConfig = {
   'apiKey': os.getenv("FIREBASE_API_KEY"),
   'authDomain': "htbn-final-project.firebaseapp.com",
   'projectId': "htbn-final-project",
-  'storageBucket': "htbn-final-project.appspot.com",
+  'storageBucket': "htbn-final-project.firebasestorage.app",
   'messagingSenderId': "85744476044",
   'appId': "1:85744476044:web:c622fa9a21d442880b40a9",
   'databaseURL': ""
 }
 
 def createVS():
-    firebase = pyrebase.initialize_app(firebaseConfig)
-    storage = firebase.storage()
+  firebase = pyrebase.initialize_app(firebaseConfig)
+  storage = firebase.storage()
 
-    pdf_path = "./protocolo2.pdf"
-    
-    # Verificar si el archivo ya existe antes de descargarlo
-    if not os.path.exists(pdf_path):
-        storage.child("protocolo2.pdf").download(filename=pdf_path, path="./")
+  storage.child("protocolo2.pdf").download(filename="protocolo2.pdf", path="./")
 
-    return vectorstore(pdf_path)
+  return vectorstore('./protocolo2.pdf')
