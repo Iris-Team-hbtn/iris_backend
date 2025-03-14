a325b71a6528f9f9c6a20359f268bb3ee025ed2e
Axel Palombo <140984664+axelpalwo@users.noreply.github.com>
2025-03-13 21:25:27 -0300

    ssl update

--- run.py
+++ run.py
@@ -3,4 +3,7 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True, ssl_context=(
+        '/etc/letsencrypt/live/34.44.177.109.nip.io/fullchain.pem',
+        '/etc/letsencrypt/live/34.44.177.109.nip.io/privkey.pem'
+    ))
